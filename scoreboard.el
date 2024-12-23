70a1d69c32d25bd746e681fcdec97a0fe744c0a6
Marcos Andrade <mnandrade1999@gmail.com>
2024-12-22 20:49:35 -0600

    transfer from emacs.org

--- scoreboard.el
+++ scoreboard.el
@@ -11,5 +11,46 @@
 ;;; Code:
 (provide 'scoreboard)
 
+
+
+  ;;(defun task-tracker-for-modeline()
+  ;;  "Return a string"
+  ;; (format  "Tasks: %d/%d" tasks-completed-for-day tasks-goal-for-day))
+
+
+	;;  (defvar tasks-goal-for-day 5 "goal number of tasks for a day")
+	;;  (defvar tasks-completed-for-day 0 "actual number of tasks completed for a day")
+	;;  (defvar last-check-date (calendar-current-date))
+
+
+
+	;;  (defun reset-task-variables-on-day-change()
+	;;    "resets"
+	;;    (unless (equal org-agenda-current-date last-check-date)
+	;;      (setq last-check-date org-agenda-current-date)
+	;;      (setq tasks-completed-for-day 0)
+	;;      (message "task tracker date has been reset")))
+
+
+	;;    (defun mr-x/task-counter ()
+	;; "Simple function to track number of tasks completed in a given day."
+	;; (interactive)
+	;; ;; Ensure reset-task-variables-on-day-change is defined
+	;; (reset-task-variables-on-day-change)
+	;; ;; Increment the counter
+	;; (cl-incf tasks-completed-for-day)
+	;; ;; Check if the task goal has been met
+	;; (if (>= tasks-completed-for-day tasks-goal-for-day)
+	;;     (message "Congrats!! You met your task completion goal for today")
+	;;   (progn
+	;;     (sit-for 2)
+	;;     (message "Tasks completed today: %d/%d" tasks-completed-for-day tasks-goal-for-day)
+	;;     (sit-for 2))))
+
+
+	;;  (add-to-list 'org-after-todo-state-change-hook
+	;; 	   (lambda ()
+	;; 	     (when (equal org-state "DONE")
+	;; 	       (mr-x/task-counter))))
 ;;; scoreboard.el ends here
 
